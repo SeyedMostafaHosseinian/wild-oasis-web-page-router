7d3dc416d23b278fe12c5887c297b92169e5337a
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2024-10-27 13:11:20 +0330

    render cabin details dynamically

--- pages/cabins/[cabinId].tsx
+++ pages/cabins/[cabinId].tsx
@@ -1,20 +1,27 @@
+import CabinView from "@/components/CabinView";
+import { getCabin } from "@/lib/data-service";
 import { generateTitle } from "@/lib/utils";
 import { TITLES_ENUM } from "@/types/constants/titles-enum";
 import Head from "next/head";
 import { useParams } from "next/navigation";
 import React from "react";
 
-export default function Cabin() {
-  const params = useParams();
-  console.log(params);
+export async function getServerSideProps({
+  params,
+}: {
+  params: Record<string, any>;
+}) {
+  const cabin = await getCabin(params?.cabinId);
+  return { props: { cabin } };
+}
+
+export default function Cabin({ cabin }: { cabin: Record<string, any> }) {
   return (
     <>
       <Head>
-        <title>
-          {generateTitle(TITLES_ENUM.CABIN + " #" + params?.cabinId)}
-        </title>
+        <title>{generateTitle(TITLES_ENUM.CABIN + " #" + cabin?.id)}</title>
       </Head>
-      <div>Cabin</div>
+      <CabinView cabin={cabin} />
     </>
   );
 }
